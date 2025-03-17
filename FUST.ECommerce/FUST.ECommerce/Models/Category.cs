81220a3bbf13961d313d0e3897cf2f5910576341
TurboWrestler250 <71407691+TurboWrestler250@users.noreply.github.com>
2025-03-17 11:01:12 +0100

    NuGet Package installed

---
+++ FUST.ECommerce/FUST.ECommerce/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace FUST.ECommerce.Models;
+
+public class Category
+{
+    private static int idUser = 0;
+    public int Id { get; set; }
+    public string Name { get; set; } = $"User-{++idUser}";
+}
